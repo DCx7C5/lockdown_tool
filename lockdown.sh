6f4fe8972901cdb69b6e2211066cc06e1a0367e6
daen <@dcx7c5>
2023-06-24 12:25:19 +0200

    lockdown.sh updated

--- lockdown.sh
+++ lockdown.sh
@@ -31,96 +31,122 @@ CFG_MP_MISC=${MODPROBE_DIR}30_${APP_NAME}_misc.conf
 
 ALL_CFG_PATHS="$CFG_SYSCTL $CFG_MP_NETPROT $CFG_MP_FS $CFG_MP_NETFS $CFG_MP_MISC $DEFAULT_SETTINGS_FILE"
 
-logtofile () {
+logtofile() {
   echo "$(date +"%Y-%m-%d %H:%M:%S") | $1" >> /var/log/lockdown.log 2>&1 &
 }
 
-shutdown () {
-  logtofile 'Exiting Lockdown Tool.'
+shutdown() {
+  logtofile 'EXIT | Exiting Lockdown Tool.'
   exit 0
 }
 
-cleanup () {
+cleanup() {
   clear
   [[ -d /tmp/lockdown ]] && rm -rf /tmp/lockdown >/dev/null
 }
 
-on_proc_interrupt () {
-  logtofile '...SIGINT received, cleaning up'
+on_proc_interrupt() {
+  logtofile 'ERROR | ...SIGINT received, cleaning up'
   cleanup
   shutdown
 }
 
-cfg_file_check () {
+cfg_file_check() {
   local cfg_path="$1"
   if [[ ! -f $cfg_path ]]; then
-    logtofile "Config file not found or empty: $cfg_path"
+    logtofile "SETUP | Config file not found or empty: $cfg_path"
     return 1
   fi
   return 0
 }
 
-create_cfg_file () {
+create_cfg_file() {
   local x cfg cfg_path="$1"
   if [[ $cfg_path == */defaults.conf ]]; then
     [[ ! -d $SETTINGS_DIR ]] && mkdir "$SETTINGS_DIR"
     echo "# LOCKDOWN SYSCTL DEFAULTS" | tee "$cfg_path" >/dev/null
     while read -r x; do
       cfg=$(echo "$x"| cut -d ',' -f2)
-      echo "#$(sysctl "$cfg")" | tee -a "$cfg_path" >/dev/null &
-    done < "templates/recommended_sysctl.conf"
+      echo "#$(sysctl "$cfg")" | tee -a "$cfg_path" >/dev/null
+    done <templates/recommended_sysctl.conf
     sed -i 's/\ \=\ /\=/;s/\t/\ /g' "$cfg_path"
     chmod 600 "$cfg_path"
   elif [[ $cfg_path == *hardening.conf ]]; then
     touch "$cfg_path"
+    while read -r x; do
+      cfg=$(echo "$x"| cut -d ',' -f2)
+      rval=$(echo "$x"| cut -d ',' -f3)
+    echo "#$cfg=$rval" | tee -a "$cfg_path" >/dev/null
+    done <templates/recommended_sysctl.conf
     chmod 644 "$cfg_path"
   elif [[ $cfg_path == *modprobe* ]]; then
-    touch "$cfg_path"
+    file=$(echo "$cfg_path"| cut -d'/' -f4)
+    cp "templates/$file" "$cfg_path"
     chmod 644 "$cfg_path"
   fi
   chown root:root "$cfg_path"
-  logtofile "Created config file: $cfg_path"
+  logtofile "SETUP | Created config file: $cfg_path"
 }
 
-help () {
+help() {
   clear
   echo "Usage: lockdown.sh                 # run interactive
                               -h | --help   # show this help message"
 }
 
-create_project_arrays () {
-  local line ks info mpnetlst mpfslst mpnetfslst mpmisclst rval dval
+create_project_arrays() {
+  local line ks info rval dval state
   declare -Ag lkm_arr sysctl_arr
   declare -ag sysc_name_arr
 
-  mpnetlst="dccp sctp rds tipc n-hdlc ax25 netrom x25 rose decnet econet af_802154 ipx appletalk psnap p8023 p8022 can atm"
-  mpfslst="cramfs freevxfs jffs2 hfs hfsplus squashfs udf"
-  mpnetfslst="cifs nfs nfsv3 nfsv4 ksmbd gfs2"
-  mpmisclst="vivid bluetooth btusb uvcvideo"
+  while read -r line; do
+    name=$(echo "$line"| sed 's/#install\ //;s/\ \/bin\/false//')
+    lkm_arr[$name]=$CFG_MP_NETPROT
+    lkm_arr[$CFG_MP_NETPROT]+="$name "
+  done <templates/30_lockdown_network_protocols.conf
 
-  for name in $mpnetlst; do lkm_arr[$name]=$CFG_MP_NETPROT; done
-  for name in $mpfslst; do lkm_arr[$name]=$CFG_MP_FS; done
-  for name in $mpnetfslst; do lkm_arr[$name]=$CFG_MP_NETFS; done
-  for name in $mpmisclst; do lkm_arr[$name]=$CFG_MP_MISC; done
+  while read -r line; do
+    name=$(echo "$line"| sed 's/#install\ //;s/\ \/bin\/false//')
+    lkm_arr[$name]=$CFG_MP_FS
+    lkm_arr[$CFG_MP_FS]+="$name "
+  done <templates/30_lockdown_filesystems.conf
 
-  lkm_arr[$CFG_MP_MISC]=$mpmisclst
-  lkm_arr[$CFG_MP_NETFS]=$mpnetfslst
-  lkm_arr[$CFG_MP_FS]=$mpfslst
-  lkm_arr[$CFG_MP_NETPROT]=$mpnetlst
+  while read -r line; do
+    name=$(echo "$line"| sed 's/#install\ //;s/\ \/bin\/false//')
+    lkm_arr[$name]=$CFG_MP_NETFS
+    lkm_arr[$CFG_MP_NETFS]+="$name "
+  done <templates/30_lockdown_network_filesystems.conf
+
+  while read -r line; do
+    name=$(echo "$line"| sed 's/#install\ //;s/\ \/bin\/false//')
+    lkm_arr[$name]=$CFG_MP_MISC
+    lkm_arr[$CFG_MP_MISC]+="$name "
+  done <templates/30_lockdown_misc.conf
 
   while read -r line; do
     info=$(echo "$line"| cut -d',' -f1)
     ks=$(echo "$line"| cut -d',' -f2)
     rval=$(echo "$line"| cut -d',' -f3)
-    dval=$(grep "$ks" "$DEFAULT_SETTINGS_FILE"| cut -d'=' -f2)
-    sysctl_arr[$ks]="$info|$rval|$dval"
+    ks_line=$(grep "$ks" "$CFG_SYSCTL")
+    dval=$(grep "#$ks" "$DEFAULT_SETTINGS_FILE"| cut -d'=' -f2)
+    if [[ $rval == "$dval" ]]; then
+      state="default,is hardened"
+    elif [[ rval != "$dval" ]] && [[ '#' == "${ks_line:0:1}" ]];then
+      state="not hardened"
+    elif [[ rval != "$dval" ]] && [[ ! '#' == "${ks_line:0:1}" ]];then
+      state="is hardened"
+    else
+      state="not hardened"
+    fi
+    sysctl_arr[$ks]="$info|$rval|$dval|$state"
     sysc_name_arr+=("$ks")
-  done < "templates/recommended_sysctl.conf"
-  logtofile "INIT | ... arrays initialized"
+  done <templates/recommended_sysctl.conf
+
+  logtofile "INIT | ... arrays created & filled"
 }
 
-init_routine () {
-  logtofile "Starting lockdown hardening and privacy tool"
+init_routine() {
+  logtofile "INIT | Starting lockdown hardening and privacy tool"
   for cfg in $ALL_CFG_PATHS; do
     if ! cfg_file_check "$cfg"; then
       create_cfg_file "$cfg"
@@ -129,7 +155,7 @@ init_routine () {
   create_project_arrays
 }
 
-lkm_is_blacklisted () {
+lkm_is_blacklisted() {
   local lkm="$1"
   lkm_line=$(grep "$lkm" "${lkm_arr[$lkm]}")
   if [[ -z $lkm_line ]]; then
@@ -141,39 +167,47 @@ lkm_is_blacklisted () {
   return 0
 }
 
-set_sysctl_hardening_state () {
-  local ks="$1" rval ks_line dval
-  if ks_is_hardened "$ks"; then
-    ks_line=$(grep "$ks" "$DEFAULT_SETTINGS_FILE")
-    dval="$(echo "$ks_line"| cut -d'=' -f2)"
-    sed -i "s/$ks/#\ $ks/" "$CFG_SYSCTL" >/dev/null &
-    sysctl -w "$ks=$dval" >/dev/null &
-    logtofile "SYSCTL | set to default @ $ks=$dval"
-  else
-    rval=$(echo "${sysctl_arr[$ks]}" | cut -d'|' -f2)
-    sed -i "s/#\ $ks/$ks/" "$CFG_SYSCTL" >/dev/null &
+set_sysctl_hardening_state() {
+  local ks="$1" rval ks_line dval state
+  state=$(echo "${sysctl_arr[$ks]}"| cut -d'|' -f4)
+  rval=$(echo "${sysctl_arr[$ks]}"| cut -d'|' -f2)
+  dval=$(echo "${sysctl_arr[$ks]}"| cut -d'=' -f3)
+  if [[ $state == 'default,is hardened' ]]; then
+    sysctl_arr[$ks]="$(echo "${sysctl_arr[$ks]}"| cut -d'|' -f'1 2 3')|file,is hardened"
+    sed -i "s/#$ks/$ks/" "$CFG_SYSCTL" >/dev/null
+    logtofile "SYSCTL | default cfg overwrite @ $ks=$rval"
+  elif [[ $state == 'not hardened' ]];then
+    sysctl_arr[$ks]="$(echo "${sysctl_arr[$ks]}"| cut -d'|' -f'1 2 3')|is hardened"
+    sed -i "s/#$ks/$ks/" "$CFG_SYSCTL" >/dev/null
     sysctl -w "$ks=$rval" >/dev/null &
     logtofile "SYSCTL | hardened settings @ $ks=$rval"
+  elif [[ $state == 'is hardened' ]];then
+    sysctl_arr[$ks]="$(echo "${sysctl_arr[$ks]}"| cut -d'|' -f'1 2 3')|not hardened"
+    sed -i "s/$ks/#$ks/" "$CFG_SYSCTL" >/dev/null
+    sysctl -w "$ks=$dval" >/dev/null &
+    logtofile "SYSCTL | set to default @ $ks=$dval"
+  elif [[ $state == 'file,is hardened' ]]; then
+      sysctl_arr[$ks]="$(echo "${sysctl_arr[$ks]}"| cut -d'|' -f'1 2 3')|default,is hardened"
+      sed -i "s/$ks/#$ks/" "$CFG_SYSCTL" >/dev/null
+      logtofile "SYSCTL | default cfg restored @ $ks=$rval"
   fi
 }
 
-ks_is_hardened () {
-  local ks="$1" ks_line ks_rec
-  ks_line=$(grep "$ks" "$CFG_SYSCTL")
-  ks_rec=$(echo "${sysctl_arr[$ks]}"| cut -d'|' -f2)
-  liveval=$(sysctl "$ks" | cut -d'=' -f2)
-  if [[ -z $ks_line ]]; then
-    echo "# $ks=$ks_rec" | tee -a "$CFG_SYSCTL" >/dev/null &
-    return 1
-  elif [[ '#' == "${ks_line:0:1}" ]]; then
-    return 1
-  elif [[ $ks_rec == "$liveval" ]]; then
-    return 0
+get_ks_state_colored() {
+  local ks="$1"
+    state=$(echo "${sysctl_arr[$ks]}"| cut -d'|' -f4)
+  if [[ $state == 'default,is hardened' ]]; then
+    echo "default,${BGREEN}is hardened${ENDC}"
+  elif [[ $state == 'not hardened' ]];then
+    echo "${BRED}not hardened${ENDC}"
+  elif [[ $state == 'is hardened' ]];then
+    echo "${BGREEN}is hardened${ENDC}"
+  elif [[ $state == 'file,is hardened' ]]; then
+    echo "${BGREEN}is hardened${ENDC}"
   fi
-  return 0
 }
 
-build_kloak () {
+build_kloak() {
   BLDDIR="/tmp/kloak"
   if [[ -d "$BLDDIR" ]]; then rm -rf "$BLDDIR" 2>/dev/null; fi
   git clone "https://github.com/vmonaco/kloak" "$BLDDIR" && cd "$BLDDIR" || exit 1
@@ -183,7 +217,7 @@ build_kloak () {
   logtofile "KLOAK | ... build successful"
 }
 
-install_kloak_service () {
+install_kloak_service() {
   cp templates/kloak.service /etc/systemd/system/kloak.service
   systemctl daemon-reload
   systemctl enable kloak.service
@@ -195,7 +229,7 @@ install_kloak_service () {
   logtofile "KLOAK | ... installed as service"
 }
 
-kloak_is_installed () {
+kloak_is_installed() {
   if [[ -f /etc/systemd/system/kloak.service ]]; then
     return 0
   else
@@ -203,7 +237,7 @@ kloak_is_installed () {
   fi
 }
 
-kloak_is_active () {
+kloak_is_active() {
   if [[ 'active' == $(systemctl is-active kloak.service) ]]; then
     return 0
   else
@@ -211,7 +245,7 @@ kloak_is_active () {
   fi
 }
 
-install_or_start_kloak () {
+install_or_start_kloak() {
   if ! kloak_is_installed; then
     build_kloak
     install_kloak_service
@@ -220,7 +254,7 @@ install_or_start_kloak () {
   fi
 }
 
-main_menu () {
+main_menu() {
   local ans mnl menu
   local -i theight
   theight=$(($(tput lines)-1))
@@ -242,15 +276,15 @@ main_menu () {
   esac
 }
 
-extras_menu () {
+extras_menu() {
   local ans mnl menu kloak_state
   local -i theight
   theight=$(($(tput lines)-1))
   clear
   for x in $(seq 1 "$((theight-3))"); do mnl+="\n"; done
-  if kloak_is_installed;then kloak_state+="${BGREEN}installed${ENDC} | ";else kloak_state+="${BRED}not installed${ENDC} | ";fi
-  if kloak_is_active;then kloak_state+="${BGREEN}running${ENDC}";else kloak_state+="${BRED}not running${ENDC}";fi
-  printf "${mnl}%-30s\t\t$kloak_state\n\n%s\n\n" "1) Install/Start Kloak module" "0) back to main menu"
+  if kloak_is_installed; then state="${BGREEN}installed${ENDC} | "; else state="${BRED}not installed${ENDC} | "; fi
+  if kloak_is_active; then echo -ne "$state${BGREEN}running${ENDC}"; else echo -ne "$state${BRED}not running${ENDC}"; fi
+  printf "${mnl}%-30s\t\t$kloak_state\n\n\n\n\n\n\n%s\n\n" "1) Install/Start Kloak module" "0) back to main menu"
   printf "${BGREEN}%s${ENDC}(${BYELLOW}extras${ENDC})> " "$APP_NAME"
   read -r ans
   case $ans in
@@ -260,28 +294,27 @@ extras_menu () {
   esac
 }
 
-ksettings_menu () {
-  local ans l elem info re line
+ksettings_menu() {
+  local ans l elem info re line state cstate
   local -i elemc count space theight
   elemc="${#sysctl_arr[@]}"
   theight=$(tput lines)
   count=1
   clear
-  for x in $(seq 1 "$((theight-elemc))"); do mnl+="\n"; done
-  echo -ne "$mnl"
+  echo -ne "\n\n"
   for l in ${sysc_name_arr[*]}; do
     elem="${sysctl_arr[$l]}"
     info=$(echo "$elem"| cut -d'|' -f1)
-    if ks_is_hardened "$l"; then val="${BGREEN}is hardened${ENDC}" ; else val="${BRED}not hardened${ENDC}"; fi
+    state=$(echo "$elem"| cut -d'|' -f4)
+    cstate=$(get_ks_state_colored "$l")
     if ((count < 10)); then space=51; else space=50; fi
-    printf "$count) $BYELLOW%-${space}s$ENDC | %-45s | $val\n" "$info" "$l"
+    printf "$count) $BYELLOW%-${space}s$ENDC | %-45s | $cstate\n" "$info" "$l"
     count=$(("$count"+1))
   done
   printf "\n%s\n%s\n" "n) harden setting / load default" "0) back to Main Menu"
   printf "\n${BGREEN}%s${ENDC}(${BYELLOW}sysctl${ENDC})> " "$APP_NAME"
   read -r ans
-  re="^1$"
-  for n in $(seq 2 "$elemc"); do re+="|^$n$"; done
+  for n in $(seq 1 "$elemc"); do re+="|^$n$"; done
   if [[ $ans == "0" ]]; then
     main_menu
   elif [[ $ans == "back" ]]; then
@@ -297,9 +330,7 @@ ksettings_menu () {
   fi
 }
 
-
-
-main () {
+main() {
   if [[ $# -eq 0 ]]; then
     init_routine
     main_menu
